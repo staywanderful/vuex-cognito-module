3f1214c934c336b36900a7ffb492d9d36fa70a21
Jonathan Alexander <jonathandalexander@gmail.com>
2019-11-23 15:52:48 -0500

    add optional type undefined in config inputs

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare module 'vuex-cognito-module' {
   interface CognitoConfig {
-    userPoolId: string;
-    identityPoolId: string;
-    userPoolWebClientId: string;
-    region: string;
+    userPoolId: string | undefined;
+    identityPoolId: string | undefined;
+    userPoolWebClientId: string | undefined;
+    region: string | undefined;
   }
 
   export default function (store: object, config: CognitoConfig, namespace?: string, vuexModuleOptions?: object): void
