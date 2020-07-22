1a8c0313b3d5a97591b9918688d2355e03b48c2e
shichan <chan.shiela+work@gmail.com>
2020-07-22 22:55:03 +0800

    add unset user to fix session isLoggedin true bug

--- src/mutations.js
+++ src/mutations.js
@@ -2,5 +2,9 @@ export default {
   setUser: (state, user) => {
     state.user = { ...user }
     state.session = state.user.signInUserSession
+  },
+  unsetUser: (state, user) => {
+    state.user = {}
+    state.session = {}
   }
 }
